5a02e63724fe5cb6479ff948e8dfbd395bd6fb26
dusavinay <dusavinay@vinay>
2018-03-26 00:11:47 +0530

    new commit

--- src/main/java/Controllers/BonafiedController.java
+++ src/main/java/Controllers/BonafiedController.java
@@ -1,6 +1,7 @@
 package Controllers;
 
 import org.hibernate.classic.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,18 +9,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.Pojo.Bonafied;
 
+import Daos.Bonafieddao;
 import Daos.DbUtil;
 
 @Controller
 public class BonafiedController {
 	
+	@Autowired
+	Bonafieddao dao;
 	
 	@RequestMapping(value="bonafied",method=RequestMethod.GET)
 	public String register(Bonafied bonafied,Model model) {
-		Session session = DbUtil.getSessionFactory().openSession();
-		session.save(bonafied);
-		session.beginTransaction().commit();
-		session.close();
+		dao.register(bonafied);
 		model.addAttribute("message", "Successfully Registered");
 		return "bonafied";
 	}
