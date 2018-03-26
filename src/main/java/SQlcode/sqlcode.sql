0621b82bea2a9860a559f84f0a65a72da3688a15
Ramp <Ramp@192.168.0.13>
2018-03-26 21:01:12 +0530

    After Creating table script.

--- src/main/java/SQlcode/sqlcode.sql
+++ src/main/java/SQlcode/sqlcode.sql
@@ -1,10 +1,12 @@
- `student`.CREATE DATABASE `student` /*!40100 DEFAULT CHARACTER SET latin1 */;
+ `student`.CREATE DATABASE `student`
 
-DROP TABLE IF EXISTS `student`.`bonafied`;
-CREATE TABLE  `student`.`bonafied` (
-  `stdroll` varchar(45) NOT NULL DEFAULT '',
-  `stdname` varchar(45) NOT NULL DEFAULT '',
-  `apply` varchar(45) NOT NULL DEFAULT '',
-  `branch` varchar(45) NOT NULL DEFAULT '',
-  PRIMARY KEY (`stdroll`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `student`.`student`;
+CREATE TABLE  `student`.`student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `college` varchar(255) DEFAULT NULL,
+  `emailid` varchar(255) DEFAULT NULL,
+  `fathername` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phoneno` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
